6b854ce647a87ef659a4c151f79b28edfe0d6284
Jorge Muza <jorge@praxard.com>
2019-12-21 19:09:29 -0600

    [fix] APN topic fix.

--- src/APNNotification.js
+++ src/APNNotification.js
@@ -37,7 +37,7 @@ export default class APNNotification extends BaseNotification {
     notification.badge = customNotification.badge || predefinedNotification.badge;
     notification.title = customNotification.title || predefinedNotification.title;
     notification.alert = customNotification.body || predefinedNotification.body;
-    notification.topic = this._config.topic;
+    notification.topic = this._config.provider.topic;
 
     return _.merge(notification, _config);
   }
